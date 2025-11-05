e86bbbf2847a266362f13b96f73306a382125ea6
Aspharier <codeworkashish@gmail.com>
2025-11-05 16:55:23 +0530

    Auth UI complete

--- frontend/app/(auth)/register.tsx
+++ frontend/app/(auth)/register.tsx
@@ -1,20 +1,111 @@
-import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
-import React from "react";
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
+import React, { useRef, useState } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import BackButton from "@/components/BackButton";
+import Input from "@/components/Input";
+import * as Icons from "phosphor-react-native";
+import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import Button from "@/components/Button";
 
 const Register = () => {
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (!emailRef.current || !passwordRef.current || !nameRef.current) {
+      Alert.alert("Sign Up", "Please fill all the fields");
+      return;
+    }
+
+    // good to go
+  };
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
       behavior={Platform.OS == "ios" ? "padding" : "height"}
     >
       <ScreenWrapper showPattern={true}>
-        <View style={styles.container} >
+        <View style={styles.container}>
           <View style={styles.header}>
-            <BackButton />
+            <BackButton iconSize={28} />
+            <Typo size={17} color={colors.white}>
+              Need some help?
+            </Typo>
+          </View>
+          <View style={styles.content}>
+            <ScrollView
+              contentContainerStyle={styles.form}
+              showsVerticalScrollIndicator={false}
+            >
+              <View style={{ gap: spacingY._10, marginBottom: spacingY._15 }}>
+                <Typo size={28} fontWeight={"600"}>
+                  Getting Started
+                </Typo>
+                <Typo color={colors.neutral600}>
+                  Create an account to continue
+                </Typo>
+              </View>
+              <Input
+                placeholder="Enter your name"
+                onChangeText={(value: string) => (nameRef.current = value)}
+                icon={
+                  <Icons.UserIcon
+                    size={verticalScale(26)}
+                    color={colors.neutral600}
+                  />
+                }
+              />
+              <Input
+                placeholder="Enter your email"
+                onChangeText={(value: string) => (emailRef.current = value)}
+                icon={
+                  <Icons.AtIcon
+                    size={verticalScale(26)}
+                    color={colors.neutral600}
+                  />
+                }
+              />
+              <Input
+                placeholder="Enter your password"
+                secureTextEntry
+                onChangeText={(value: string) => (passwordRef.current = value)}
+                icon={
+                  <Icons.LockIcon
+                    size={verticalScale(26)}
+                    color={colors.neutral600}
+                  />
+                }
+              />
+              <View style={{ marginTop: spacingX._25, gap: spacingY._15 }}>
+                <Button loading={isLoading} onPress={handleSubmit}>
+                  <Typo fontWeight={"bold"} color={colors.black} size={20}>
+                    Sign Up
+                  </Typo>
+                </Button>
+                <View style={styles.footer}>
+                  <Typo>Already have an account?</Typo>
+                  <Pressable onPress={() => router.push("/(auth)/login")}>
+                    <Typo fontWeight={"bold"} color={colors.primaryDark}>
+                      Login
+                    </Typo>
+                  </Pressable>
+                </View>
+              </View>
+            </ScrollView>
           </View>
         </View>
       </ScreenWrapper>
--- frontend/components/BackButton.tsx
+++ frontend/components/BackButton.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { colors } from "@/constants/theme";
+import { BackButtonProps } from "@/types";
+import { useRouter } from "expo-router";
+import { CaretLeftIcon } from "phosphor-react-native";
+import { verticalScale } from "@/utils/styling";
 
-const BackButton = () => {
+const BackButton = ({
+  style,
+  iconSize = 26,
+  color = colors.white,
+}: BackButtonProps) => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>BackButton</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      onPress={() => router.back()}
+      style={[styles.button, style]}
+    >
+      <CaretLeftIcon
+        size={verticalScale(iconSize)}
+        color={color}
+        weight="bold"
+      />
+    </TouchableOpacity>
+  );
+};
 
-export default BackButton
+export default BackButton;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button: {},
+});
