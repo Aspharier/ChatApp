6058e0e61fcec4418ed63583caed92f3c903ed06
Aspharier <codeworkashish@gmail.com>
2025-11-17 21:55:01 +0530

    Added Profile Edit screen with backend support / Avatar Cloudinary upload done

--- backend/socket/userEvents.ts
+++ backend/socket/userEvents.ts
@@ -1,6 +1,50 @@
 import { Socket, Server as SocketIOServer } from "socket.io";
+import User from "../modals/User.js";
+import { generateToken } from "../utils/token.js";
 export function registerUserEvents(io: SocketIOServer, socket: Socket) {
   socket.on("testSocket", (data) => {
     socket.emit("testSocket", { msg: "Real Time Update" });
   });
+
+  socket.on(
+    "updateProfile",
+    async (data: { name?: string; avatar?: string }) => {
+      console.log("updateProfile event: ", data);
+
+      const userId = socket.data.userId;
+      if (!userId) {
+        return socket.emit("updateProfile", {
+          success: false,
+          msg: "Unauthorized",
+        });
+      }
+      try {
+        const updateUser = await User.findByIdAndUpdate(
+          userId,
+          { name: data.name, avatar: data.avatar },
+          { new: true } // will return the user with the updated values
+        );
+        if (!updateUser) {
+          return socket.emit("updateProfile", {
+            success: false,
+            msg: "User not found",
+          });
+        }
+
+        // gen token with updated values
+        const newToken = generateToken(updateUser);
+        socket.emit("updateProfile", {
+          success: true,
+          data: { token: newToken },
+          msg: "Profile updated successfully",
+        });
+      } catch (error) {
+        console.log("Error updating profile: ", error);
+        socket.emit("updateProfile", {
+          success: false,
+          msg: "Error updating profile",
+        });
+      }
+    }
+  );
 }
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,4 +1,7 @@
 import { Platform } from "react-native";
 
-export const API_URL =
-  Platform.OS === "android" ? "http://10.0.2.2:3000" : "http://localhost:3000";
+// export const API_URL = Platform.OS === "android" ? "http://10.0.2.2:3000" : "http://localhost:3000";
+
+export const API_URL = Platform.OS === "android"? "http://10.54.31.4:3000" : "http://localhost:3000";
+export const CLOUDINARY_CLOUD_NAME = "dij8r1uk4";
+export const CLOUDINARY_UPLOAD_PRESET = "images";
