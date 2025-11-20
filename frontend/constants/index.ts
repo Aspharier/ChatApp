004ec908e3d8179c578a737f4549c00bdaee9467
Aspharier <codeworkashish@gmail.com>
2025-11-20 11:53:36 +0530

    Final Push

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -2,6 +2,7 @@ import { Platform } from "react-native";
 
 // export const API_URL = Platform.OS === "android" ? "http://10.0.2.2:3000" : "http://localhost:3000";
 
-export const API_URL = Platform.OS === "android"? "http://192.168.31.126:3000" : "http://localhost:3000";
+// export const API_URL = Platform.OS === "android"? "http://192.168.31.126:3000" : "http://localhost:3000";
+export const API_URL = "https://chatapp-cg8c.onrender.com";
 export const CLOUDINARY_CLOUD_NAME = "dij8r1uk4";
 export const CLOUDINARY_UPLOAD_PRESET = "images";
